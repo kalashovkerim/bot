6c6004f1b41f233b844e9e254781258c00201d13
kalashovkerim <91393656+kalashovkerim@users.noreply.github.com>
2022-03-23 04:14:18 +0400

    Add files via upload

---
+++ cvaribles.py
@@ -0,0 +1,16 @@
+password_success = False
+name_success = False
+subject_success = False
+
+add_group_activated = False
+delete_group_activated = False
+
+student_group_buttons = False
+lesson_control_buttons = False
+add_student_activated = False
+delete_student_activated = False
+
+
+group_index = -1
+
+crease_index = -1
